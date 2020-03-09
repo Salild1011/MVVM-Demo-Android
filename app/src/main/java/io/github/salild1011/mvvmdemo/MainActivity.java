9a4d1d25007f9d40214a8baace2c3bfe5bdd66a0
Salild1011 <salild1011@gmail.com>
2020-03-08 22:41:46 -0400

    Added Launcher activity
    Signed-off-by: Salild1011 <salild1011@gmail.com>

---
+++ app/src/main/java/io/github/salild1011/mvvmdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.salild1011.mvvmdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
