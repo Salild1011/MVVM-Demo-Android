a97fd2df0dfd9906294bb33bababd5bd24fa6aba
Salild1011 <salild1011@gmail.com>
2020-03-08 23:26:41 -0400

    Set up view bindings
    Signed-off-by: Salild1011 <salild1011@gmail.com>

--- app/src/main/java/io/github/salild1011/mvvmdemo/MainActivity.java
+++ app/src/main/java/io/github/salild1011/mvvmdemo/MainActivity.java
@@ -1,14 +1,25 @@
 package io.github.salild1011.mvvmdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view) private RecyclerView recyclerView;
+    @BindView(R.id.progress_circular) private ProgressBar progressBar;
+    @BindView(R.id.floating_action_button) private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
