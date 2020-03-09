92203100de834a4a4f05eb001a95410e3edce181
Salild1011 <salild1011@gmail.com>
2020-03-08 23:26:41 -0400

    RecyclerView and ProgressBar setup methods added
    Signed-off-by: Salild1011 <salild1011@gmail.com>

--- app/src/main/java/io/github/salild1011/mvvmdemo/MainActivity.java
+++ app/src/main/java/io/github/salild1011/mvvmdemo/MainActivity.java
@@ -1,14 +1,46 @@
 package io.github.salild1011.mvvmdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.salild1011.mvvmdemo.adapters.RecyclerAdapter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view) private RecyclerView recyclerView;
+    @BindView(R.id.progress_circular) private ProgressBar progressBar;
+    @BindView(R.id.floating_action_button) private FloatingActionButton floatingActionButton;
+
+    private RecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    private void initRecyclerView() {
+        recyclerAdapter = new RecyclerAdapter(new ArrayList<>(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    private void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
     }
 }
