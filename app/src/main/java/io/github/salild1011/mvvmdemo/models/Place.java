0663ae3b34db31db8e689585aab28f1f3635e06f
Salild1011 <salild1011@gmail.com>
2020-03-08 22:42:55 -0400

    A model for place data
    Signed-off-by: Salild1011 <salild1011@gmail.com>

---
+++ app/src/main/java/io/github/salild1011/mvvmdemo/models/Place.java
@@ -0,0 +1,28 @@
+package io.github.salild1011.mvvmdemo.models;
+
+public class Place {
+
+    private String title;
+    private String imageUrl;
+
+    public Place(String title, String imageUrl) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
