fb56ef463485c1c6ae8c26bc9d2e38b60714c8ce
Salild1011 <salild1011@gmail.com>
2020-03-09 21:44:26 -0400

    Added updating status
    Signed-off-by: Salild1011 <salild1011@gmail.com>

--- app/src/main/java/io/github/salild1011/mvvmdemo/viewmodels/MainActivityViewModel.java
+++ app/src/main/java/io/github/salild1011/mvvmdemo/viewmodels/MainActivityViewModel.java
@@ -1,5 +1,7 @@
 package io.github.salild1011.mvvmdemo.viewmodels;
 
+import android.os.AsyncTask;
+
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
@@ -12,6 +14,7 @@ public class MainActivityViewModel extends ViewModel {
 
     private MutableLiveData<List<Place>> places;
     private PlaceRepository placeRepo;
+    private MutableLiveData<Boolean> isUpdating = new MutableLiveData<>();
 
     public void init() {
         if (places != null) {
@@ -22,7 +25,37 @@ public class MainActivityViewModel extends ViewModel {
         places = placeRepo.getPlaces();
     }
 
+    public void addNewValue(final Place place) {
+        isUpdating.setValue(true);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+                List<Place> currentPlaces = places.getValue();
+                currentPlaces.add(place);
+
+                places.postValue(currentPlaces);
+                isUpdating.setValue(false);
+            }
+        }.execute();
+    }
+
     public LiveData<List<Place>> getPlaces() {
         return places;
     }
+
+    public LiveData<Boolean> getIsUpdating() { return isUpdating; }
 }
